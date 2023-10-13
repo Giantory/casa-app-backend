aa06d4421be5953ce4c9590ccfad6aebe011c48f
Giantory <giantory.bryant@gmail.com>
2023-10-13 10:36:51 -0500

    add mysql database connection

---
+++ config/database.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql';
+import { config } from 'dotenv';
+config();
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+};
+
+const db = mysql.createConnection(dbConfig);
+
+// connection to database
+async () => {
+    try {
+        const connection = db.connect() 
+        console.info(`Database Connected`);
+        return connection;
+    } catch (error) {
+        console.error(`Database connection error: ${error}`);
+    }
+};
+
+export default db;

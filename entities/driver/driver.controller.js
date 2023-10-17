b24c84079c1509f80337115c04c900833ab516db
Giantory <giantory.bryant@gmail.com>
2023-10-17 09:12:49 -0500

    fix error message

--- entities/driver/driver.controller.js
+++ entities/driver/driver.controller.js
@@ -17,11 +17,10 @@ export const getDriverById = (req, res) => {
     const driverId=req.params.id;
     driverUseCases.getDriverById(driverId, (err, results) => {
         if (err) {
-            console.error('Error al obtener operadores:', err);
-            res.status(500).json({ error: 'Error al obtener operadores' });
+            console.error('Error al obtener al conductor:', err);
+            res.status(500).json({ error: 'Error al obtener al conductor' });
         } else if (results.length == 0) {
             res.status(400).json({ error: 'No se encontró el conductor' });
-
         } else {
             res.json(results);
         }

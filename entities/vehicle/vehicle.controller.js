9e066095bedb912f33e4fd8e19ff929b07adeaa1
Giantory <gian.s@sistemasparaempresas.com>
2024-06-21 17:25:02 -0500

    adding a new query to get new consumption information per vehicle

--- entities/vehicle/vehicle.controller.js
+++ entities/vehicle/vehicle.controller.js
@@ -27,6 +27,20 @@ export const getVehicleByCode = (req, res) => {
     });
 }
 
+export const getVehicleDetailsByCode = (req, res) => {
+    const code = req.params.code;
+    vehicleUseCases.getVehicleDetailsByCode(code, (err, results) => {
+        if (err) {
+            console.error('Error al obtener los detalles vehículo:', err);
+            res.status(500).json({ error: 'Error al obtener los detalles del vehículo' });
+        } else if (results.length == 0) {
+            res.status(400).json({ error: 'No se encontró el vehículo' });
+        } else {
+            res.json(results);
+        }
+    });
+}
+
 export const updateVehicle = async (req, res) => {
     const code = req.params.code;
 
@@ -44,6 +58,25 @@ export const updateVehicle = async (req, res) => {
     } catch (error) {
         console.error('Error al procesar el consumo', error);
         res.status(500).json({ error: 'Error al procesar el consumo' });
+    }
+
+}
+
+export const createVehicle = async (req, res) => {
+
+    const vehicle = req.body;
+
+    try {
+        vehicleUseCases.createVehicle(vehicle, (err, results) => {
+            if (err) {
+                res.status(500).json({ error: 'No se pudo crear el vehículo' })
+            } else {
+                res.status(200).json(results[0])
+            }
+        });
+    } catch (error) {
+        console.error('Error al crear el vehículo', error);
+        res.status(500).json({ error: 'Error al crear el vehículo' });
     }
 
 }
--- entities/vehicle/vehicle.useCases.js
+++ entities/vehicle/vehicle.useCases.js
@@ -13,9 +13,22 @@ class VehicleUseCases {
     getVehicleByCode(code, callback) {
         db.query("SELECT * FROM equipo WHERE placa=?", [code], callback);
     }
+    getVehicleDetailsByCode(code, callback) {
+        db.query("SELECT ec.descripcion AS mensajeEstado, eq.*, co.*, de.* FROM equipo eq "+ 
+            "INNER JOIN consumo co ON co.placa = eq.placa "+
+            "INNER JOIN despacho de ON de.idDespacho = co.idDespacho " +
+            "INNER JOIN estadoconsum ec ON ec.idEstado = co.idEstadoConsum " +
+            "WHERE eq.placa=? "+
+            "ORDER BY de.fechaDespacho;", [code], callback);
+    }
 
     updateVehicleHMKMByCode(code, horometraje, kilometraje, callback) {
         db.query("UPDATE equipo SET horometraje=?, kilometraje=? WHERE placa=?", [horometraje, kilometraje, code], callback);
+    }
+    createVehicle(code, callback) {
+        db.query("INSERT INTO equipo (placa, descripcion, idModelo, idMarca, idFrente, estado, horometraje, kilometraje, updated_at)" +
+            "VALUES (?,?,?,?,?,?,?,?,?)",
+            [code], callback);
     }
 }
 
